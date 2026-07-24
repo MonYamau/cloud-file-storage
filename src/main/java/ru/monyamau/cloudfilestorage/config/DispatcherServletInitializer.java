5a743ee3ee7cf7f1d9f978cfc87f99acd03ab58a
MonYamau <White.Fox1825@yandex.ru>
2026-07-24 21:01:53 +0500

    feat: add configuration for project;

---
+++ src/main/java/ru/monyamau/cloudfilestorage/config/DispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package ru.monyamau.cloudfilestorage.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/ru/monyamau/cloudfilestorage/config/SpringConfig.java
@@ -0,0 +1,69 @@
+package ru.monyamau.cloudfilestorage.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("ru.monyamau.cloudfilestorage")
+@EnableWebMvc
+@PropertySource("classpath:db.properties")
+@PropertySource("classpath:hibernate.properties")
+@EnableJpaRepositories("ru.monyamau.cloudfilestorage.repository")
+@EnableTransactionManagement
+public class SpringConfig implements WebMvcConfigurer {
+    private final Environment env;
+
+    @Autowired
+    public SpringConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver_class"));
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+        return dataSource;
+    }
+
+    public Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean managerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        managerFactoryBean.setDataSource(dataSource());
+        managerFactoryBean.setPackagesToScan("ru.monyamau.cloudfilestorage.model");
+        managerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        managerFactoryBean.setJpaProperties(hibernateProperties());
+        return managerFactoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
