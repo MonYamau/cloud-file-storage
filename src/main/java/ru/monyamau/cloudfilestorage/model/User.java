cd5ee1ffdefa17a898425e9c9720021003cf329d
MonYamau <White.Fox1825@yandex.ru>
2026-07-25 14:34:05 +0500

    feat: add new entity;

---
+++ src/main/java/ru/monyamau/cloudfilestorage/model/User.java
@@ -0,0 +1,28 @@
+package ru.monyamau.cloudfilestorage.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "NAME", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
