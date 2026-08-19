4b0f80129231a3b42d78e6fe1714c9b2a45c5a15
MonYamau <White.Fox1825@yandex.ru>
2026-08-19 17:35:59 +0500

    feat: add configuration for multipart-requests;

--- src/main/java/ru/monyamau/cloudfilestorage/config/DispatcherServletInitializer.java
+++ src/main/java/ru/monyamau/cloudfilestorage/config/DispatcherServletInitializer.java
@@ -1,9 +1,16 @@
 package ru.monyamau.cloudfilestorage.config;
 
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
 import org.jspecify.annotations.Nullable;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private static final long UNIT_MB = 1024 * 1024;
+    private static final long MAX_FILE_SIZE = UNIT_MB * 12;
+    private static final long MAX_REQUEST_SIZE = (MAX_FILE_SIZE + 2) * 3;
+    private static final long FILE_SIZE_THRESHOLD = UNIT_MB * 2;
+
     @Override
     protected Class<?> @Nullable [] getRootConfigClasses() {
         return new Class[]{ApplicationConfig.class};
@@ -17,5 +24,12 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        MultipartConfigElement configElement = new MultipartConfigElement(
+                System.getProperty("java.io.tmpdir"), MAX_FILE_SIZE, MAX_REQUEST_SIZE, (int) FILE_SIZE_THRESHOLD);
+        registration.setMultipartConfig(configElement);
     }
 }
--- src/main/java/ru/monyamau/cloudfilestorage/config/WebConfig.java
+++ src/main/java/ru/monyamau/cloudfilestorage/config/WebConfig.java
@@ -1,8 +1,11 @@
 package ru.monyamau.cloudfilestorage.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,6 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     public WebConfig(SessionInterceptor sessionInterceptor) {
         this.sessionInterceptor = sessionInterceptor;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 
     @Override
