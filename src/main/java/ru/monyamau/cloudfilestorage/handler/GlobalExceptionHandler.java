e29b2b03e092ff6e8faaa06ec27e767f9cc52682
MonYamau <White.Fox1825@yandex.ru>
2026-09-03 17:38:06 +0500

    refactor(controller advice): add exceptions to GlobalExceptionHandler

--- src/main/java/ru/monyamau/cloudfilestorage/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/monyamau/cloudfilestorage/handler/GlobalExceptionHandler.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.monyamau.cloudfilestorage.dto.response.ErrorDto;
-import ru.monyamau.cloudfilestorage.exception.AuthenticationException;
+import ru.monyamau.cloudfilestorage.exception.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDto> handleValid(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorDto> handleValidException(MethodArgumentNotValidException e) {
         ErrorDto errorDto = new ErrorDto("Any validation error");
         if (e.getBindingResult().getFieldError() != null) {
             errorDto = new ErrorDto(e.getBindingResult().getFieldError().getDefaultMessage());
@@ -20,9 +20,27 @@ public class GlobalExceptionHandler {
                 .body(errorDto);
     }
 
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<ErrorDto> handleValidException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorDto(e.getMessage()));
+    }
+
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorDto> handleAuthException(AuthenticationException e) {
+    public ResponseEntity<ErrorDto> handleAuthException(Exception e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorDto(e.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto(e.getMessage()));
+    }
+
+    @ExceptionHandler({ResourceAlreadyExistsException.class, UserAlreadyExistsException.class})
+    public ResponseEntity<ErrorDto> handleConflictException(Exception e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(new ErrorDto(e.getMessage()));
     }
 
