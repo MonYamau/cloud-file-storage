64e25d18b0e37b8d30f75cdad1c322bedf50345d
MonYamau <White.Fox1825@yandex.ru>
2026-09-07 22:27:19 +0500

    refactor(entity): synchronize entity constraints with database

--- src/main/java/ru/monyamau/cloudfilestorage/entity/User.java
+++ src/main/java/ru/monyamau/cloudfilestorage/entity/User.java
@@ -4,18 +4,20 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Check;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "users")
+@Check(constraints = "char_length(NAME) > 5 AND char_length(PASSWORD) = 60")
 public class User {
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "NAME", unique = true, nullable = false)
+    @Column(name = "NAME", length = 40, unique = true, nullable = false)
     private String name;
 
     @Column(name = "PASSWORD", length = 60, nullable = false)
